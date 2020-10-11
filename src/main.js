a2d8615a9771a12f47301cfcffbb19309437fe97
RogerMo2424 <66276861+RogerMo2424@users.noreply.github.com>
2020-10-11 18:32:57 -0400

    Added formspree functionality

--- src/main.js
+++ src/main.js
@@ -10,8 +10,15 @@ function getData(){
     });
     
     successMessage = document.querySelector(".alert-success");
+    
+    document.getElementsByName("firstName")[0].onchange = updateName;
+    document.getElementsByName("lastName")[0].onchange = updateName;
 }
 
 function displayFormSuccess(){
     successMessage.style.display = "block";
+}
+
+function updateName(){
+    document.getElementsByName("name")[0].value = document.getElementsByName("firstName")[0].value + " " + document.getElementsByName("lastName")[0].value;
 }
